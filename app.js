d5e7bc6f745b1dc101a77247790c55c6d0832472
ager001 <152964009+ager001@users.noreply.github.com>
2026-01-26 16:52:40 +0300

    feat: add admin dashboard route and middleware for logging and access control

--- app.js
+++ app.js
@@ -4,6 +4,46 @@ const PORT = process.env.PORT || 8000;// Updated to use PORT from .env file
 
 app.use(express.json());// Middleware to parse JSON bodies
 
+
+app.get('/admin-dashboard', (req,res)=>{
+    res.status(200).json({status: "OK",
+    message: "Welcome to the admin panel"
+    });
+    
+});
+
+
+app.use((req, res, next)=>{
+    console.log('Logger...');
+    console.log("This is the request method:", req.method);
+    console.log("This is the request URL:",req.url);
+    next();
+    
+
+});
+
+app.use((req, res, next)=>{
+    console.log('Adding custom Header...');
+    res.setHeader('X-Powered-By', 'Midenga');
+    
+    next();
+    
+
+});
+
+app.use((req, res, next)=>{
+    if(req.url === '/admin'){
+    console.log('Request is blocked.');
+    return res.status(403).json({message: "Access to admin is forbidden"});
+    }
+    next();
+
+});
+
+
+
+
+
 let movies = [
   {
     title: "Inception",
