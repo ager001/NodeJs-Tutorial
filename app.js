75b210c4db15bc63c1fbb3bc31caf62eede86b33
ager001 <152964009+ager001@users.noreply.github.com>
2026-01-29 17:47:50 +0300

    feat: add movies and users data, implement movie retrieval by ID

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { parse } = require('dotenv');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;// Updated to use PORT from .env file
@@ -12,7 +13,7 @@ app.get('/admin-dashboard', (req,res)=>{
     
 });
 
-
+// Custom Middleware functions
 app.use((req, res, next)=>{
     console.log('Logger...');
     console.log("This is the request method:", req.method);
@@ -46,35 +47,75 @@ app.use((req, res, next)=>{
 
 let movies = [
   {
+    id: 1,
     title: "Inception",
     year: 2010,
     genre: "Sci-Fi"
   },
   {
+    id: 2,
     title: "The Dark Knight",
     year: 2008,
     genre: "Action"
   },
   {
+    id: 3,
     title: "Interstellar",
     year: 2014,
     genre: "Sci-Fi"
   },
   {
+    id: 4,
     title: "Parasite",
     year: 2019,
     genre: "Thriller"
   },
   {
+    id: 5,
     title: "The Godfather",
     year: 1972,
     genre: "Crime"
   }
 ];
 
+let users = [
+    {
+    id: 1,    
+    name: "Midenga",
+    email: "agermidenga@gmail.com"
+    },
+    {
+        id: 2,
+        name:"Bwai",
+        email:"abrahambwai@gmail.com"
+    },
+    {
+        id: 3,
+        name:"Mao",
+        email:"zerrubabelmao@gmail.com"
+    },
+
+];
 
 app.get('/', (req, res)=>{
     res.send("Midenga NodeJs Tutorial");
+});
+
+app.get('/movies/:id', (req,res)=>{
+    console.log("Route parameter:", req.params);// always sent as string
+    const movieId = parseInt(req.params.id);//converting string to number
+    const movie = movies.find((movie) => movie.id === movieId)// Finding movie by id
+    if(!movie){
+        return res.status(404).json({
+            success:false,
+            message: `movie with id ${movieId} not found`
+        });
+    }
+    res.status(200).json({
+        success: true,
+        data: movie,
+        message: `movie with id ${movieId} found successfully`
+    });
 });
 
 app.get("/movies", (req,res)=>{
