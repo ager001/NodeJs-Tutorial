e7c561652acabbfe9a4c9404d221cec41bf90246
ager001 <152964009+ager001@users.noreply.github.com>
2026-02-10 10:09:34 +0300

    feat: add connectDb function for MongoDB connection and update dependencies

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-require('dotenv').config();
-const express = require('express');
-const movieRouter = require('./routes/movies');
-const usersRouter = require('./routes/users');
-const homePage = require('./routes/home');
-const { customHeader, blocker, logger } = require('./middleware/custom-middleware');
-const mongoose = require('mongoose');
-
-
-
+require("dotenv").config();
+const express = require("express");
+const movieRouter = require("./routes/movies");
+const usersRouter = require("./routes/users");
+const homePage = require("./routes/home");
+const {
+  customHeader,
+  blocker,
+  logger,
+} = require("./middleware/custom-middleware");
+const mongoose = require("mongoose");
+const connectDb = require("./db/connectDb");
 
 const app = express();
-const PORT = process.env.PORT || 8000;// Updated to use PORT from .env file
+const PORT = process.env.PORT || 8000; // Updated to use PORT from .env file
 
-app.use(express.json());// Middleware to parse JSON bodies
-
-
-
+app.use(express.json()); // Middleware to parse JSON bodies
 
 // Custom Middleware functions
 app.use(logger);
@@ -23,19 +22,19 @@ app.use(blocker);
 app.use(customHeader);
 
 // Routes
-app.use('/movies', movieRouter);
-app.use('/users', usersRouter);
-app.use('/',homePage);
+app.use("/movies", movieRouter);
+app.use("/users", usersRouter);
+app.use("/", homePage);
 
-
-
-
-
-
-app.listen(PORT,()=>{
-    mongoose.connect(
-        process.env.MONGODB_URI
-    );
+app.listen(PORT, async () => {
+  try {
+    console.log("Connected to database");
+    await connectDb(process.env.MONGODB_URI);
     console.log(`App is listening on port ${PORT}`);
+  } catch (error) {
+    console.log('An error occurred.');
+    console.log(error.message);
     
+    
+  }
 });
---
+++ db/connectDb.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDb = async(uri)=>{
+    try {
+        await mongoose.connect(uri);
+    } catch (error) {
+        console.log(error.message);
+        
+    }
+
+};
+
+module.exports = connectDb;
